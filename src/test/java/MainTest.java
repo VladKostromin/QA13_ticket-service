dd067c211d617ca4e06a914eb2b5919301d3907b
Vlad <stormilolz@gmail.com>
2022-07-08 01:27:34 +0300

    Homework, 1st commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MainTest {
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+    @Test
+    public void testSomething() {
+
+    }
+}
