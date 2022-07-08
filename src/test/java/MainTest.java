4feab36ba547c5f385a04f509850553170fb6514
Vlad <stormilolz@gmail.com>
2022-07-08 04:59:13 +0300

    2nd commit, homework finished

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MainTest {
+    WebDriver webDriver;
+    String url = "https://ticket-service-69443.firebaseapp.com/";
+
     @BeforeMethod
     public void preCondition() {
-
+        webDriver = new ChromeDriver();
+        webDriver.get(url);
     }
     @Test
     public void testSomething() {
